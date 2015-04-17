042c41f373a67cceb26455b7f184c3ed0e21b1ae
johnalthom13 <ajmendoza.upm@gmail.com>
2015-04-18 07:21:46 +0800

    Changed Common.java

--- com/Common.java
+++ com/Common.java
@@ -7,7 +7,6 @@ public class Common
     public static long generateRandomLong (long limit)
     {
         long i = -1;
-
         Random generator = new Random();
         while (i > limit || i < 0)
         {
@@ -18,11 +17,10 @@ public class Common
         return i;
     }
 
-    static public byte stringToByte ( String s )
+    static public byte stringToByte (String s)
     {
         int i = 0;
         byte b = 0;
-
         try
         {
             i = Integer.parseInt(s.trim());
@@ -35,29 +33,12 @@ public class Common
         return b;
     }
 
-    static public int stringToInt ( String s )
+    static public int stringToInt (String s)
     {
         int i = 0;
-
         try
         {
             i = Integer.parseInt(s.trim());
-        }
-        catch (NumberFormatException nfe)
-        {
-            System.out.println("NumberFormatException: " + nfe.getMessage());
-        }
-
-        return i;
-    }
-
-    static public long stringToLong ( String s )
-    {
-        long i = 0;
-
-        try
-        {
-            i = Long.parseLong(s.trim());
         }
         catch (NumberFormatException nfe)
         {
